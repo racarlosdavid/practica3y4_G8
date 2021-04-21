886ec48682e801f05178512c7b8086734f7b1441
carlos david <2111724640101@ingenieria.usac.edu.gt>
2021-04-20 20:40:45 -0600

    Se creo el Backend

---
+++ Backend/src/mysql_connection.js
@@ -0,0 +1,18 @@
+var mysql      = require('mysql');
+var mysqlConnection = mysql.createConnection({
+  host     : '40.124.122.8',
+  user     : 'root',
+  password : 'Tit@nium123_Us@c',
+  database : 'ayd1'
+});
+
+mysqlConnection.connect(function (err){
+    if(err){
+        console.log(' Error al conectarse a la base de datos ',err);
+    } else {
+        console.log(' Coneccion a la base de datos exitosa ');
+    }
+});
+
+
+module.exports = mysqlConnection;
