46b1963e994ade0b5dd1e7db1552a46f0d83627b
carlos david <2111724640101@ingenieria.usac.edu.gt>
2021-04-22 17:43:46 -0600

    Se agrego el modulo de pago de peliculas solo disenio sin funcionalidad

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HistorialcompraComponent } from './components/historialcompra/historialcompra.component';
+import { PagoDePeliculasComponent } from './components/pago-de-peliculas/pago-de-peliculas.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HistorialcompraComponent
+    HistorialcompraComponent,
+    PagoDePeliculasComponent
   ],
   imports: [
     BrowserModule,
