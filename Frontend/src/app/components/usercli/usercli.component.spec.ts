3b625c240572085b6cac47627bc16f6c5849606b
Pablo César <pablopachecof.18@gmail.com>
2021-04-24 04:05:10 -0600

    Se agregaron los componentes para los usuarios cliente y administrador

---
+++ Frontend/src/app/components/usercli/usercli.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { UsercliComponent } from './usercli.component';
+
+describe('UsercliComponent', () => {
+  let component: UsercliComponent;
+  let fixture: ComponentFixture<UsercliComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ UsercliComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(UsercliComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ Frontend/src/app/components/usercli/usercli.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-usercli',
+  templateUrl: './usercli.component.html',
+  styleUrls: ['./usercli.component.css']
+})
+export class UsercliComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
