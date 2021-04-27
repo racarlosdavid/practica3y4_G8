47a1e24385b49d797b6433d589d4f4c6a3640f61
Romario Castillo <2447479030101@ingenieria.usac.edu.gt>
2021-04-26 19:15:50 -0600

    Se implemento el endpoint de inventario de películas.

---
+++ Backend/src/routes/inventarioDePeliculas.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+
+const mysqlConnection = require('../mysql_connection');
+
+router.post('/',  function(req, res, next) {
+    console.log(req.body)
+    mysqlConnection.query('SELECT P.name, P.image, A.llave, A.fecha FROM Pelicula P INNER JOIN Alquiler A ON A.idpelicula = P.idpelicula WHERE A.dpi = ' + req.body.dpi + ';', function(err, rows, fields) {
+        if (err) throw err;
+        res.send(rows);
+    });
+    
+});
+
+module.exports = router;
