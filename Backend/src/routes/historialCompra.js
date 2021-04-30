915ea5bd4fbd88aa752891fa911ceb207167f27a
cristian estuardo herrera poncio <2070142720101@ingenieria.usac.edu.gt>
2021-04-29 22:44:40 -0600

    Se corrigio el endpoint HistorialCompra haci como el componente

--- Backend/src/routes/historialCompra.js
+++ Backend/src/routes/historialCompra.js
@@ -4,8 +4,8 @@ var router = express.Router();
 const mysqlConnection = require('../mysql_connection');
 
 router.post('/historialcompra',  function(req, res) {
-    mysqlConnection.query('SELECT * FROM Pago WHERE dpi = '+
-    +req.body.id+';'
+    mysqlConnection.query('select PA.Id_transaccion,PA.Num_tarjeta,PA.Nombre_tarjeta,PA.Fecha_vencimiento,PA.CVV,PA.Total,JSON_ARRAYAGG(Pelicula.name)AS attributes From Alquiler,Pelicula, (select * From Pago where dpi = '+
+    +req.body.id+ ') as PA where Alquiler.Id_transaccion = PA.Id_transaccion and Alquiler.idpelicula = Pelicula.idpelicula group by PA.Id_transaccion;'
     , function(err, rows, fields) {
         if (err) throw err;
         res.json( rows );
