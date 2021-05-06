f96fb610809cc363ea20842d80761597fb94ed61
Pablo César <pablopachecof.18@gmail.com>
2021-05-06 14:41:51 -0600

    Se implementó SonarQube para la evaluación de métricas del software

--- Frontend/src/app/components/usercli/usercli.component.spec.ts
+++ Frontend/src/app/components/usercli/usercli.component.spec.ts
@@ -18,8 +18,4 @@ describe('UsercliComponent', () => {
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
 });
